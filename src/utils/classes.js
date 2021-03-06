d3e9f5c98012806c435b1bc95cb1ed0116d62c5d
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-06 17:56:10 -0300

    Add class 'Inventor'

--- src/utils/classes.js
+++ src/utils/classes.js
@@ -1,13 +1,17 @@
 import BARBARO from './classesDetalhado/barbaro';
 import BUCANEIRO from './classesDetalhado/bucaneiro';
 import CACADOR from './classesDetalhado/cacador';
+import CAVALEIRO from './classesDetalhado/cavaleiro';
 import GUERREIRO from './classesDetalhado/guerreiro';
+import INVENTOR from './classesDetalhado/inventor';
 
 const CLASSES = [
     BARBARO,
     BUCANEIRO,
     CACADOR,
-    GUERREIRO
+    CAVALEIRO,
+    GUERREIRO,
+    INVENTOR,
 ]
 
 export default CLASSES;
