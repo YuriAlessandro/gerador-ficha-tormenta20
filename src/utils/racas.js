209af355f7f59ac85bf9f63f5de1909c61b6ca18
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-06 22:20:57 -0300

    Add race 'Dahllan'

--- src/utils/racas.js
+++ src/utils/racas.js
@@ -1,6 +1,11 @@
 import ANAO from './racasDetalhado/anao';
+import DAHLLAN from './racasDetalhado/dahllan';
 import HUMANO from './racasDetalhado/humano';
 
-const RACAS = [ANAO, HUMANO];
+const RACAS = [
+  ANAO,
+  DAHLLAN,
+  HUMANO,
+];
 
 export default RACAS;
