58532f9a7ae5857fbefefbb0ab6333444571ec8e
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-07 18:02:27 -0300

    Add Azguer

---
+++ src/utils/divindades/azgher.ts
@@ -0,0 +1,18 @@
+import Divindade from './Divindade';
+
+const AZGUER: Divindade = {
+  name: 'Azguer',
+  racas: ['Suraggel (Aggelus)', 'Qareen'],
+  classes: [
+    'Arcanista',
+    'Bárbaro',
+    'Caçador',
+    'Cavaleiro',
+    'Guerreiro',
+    'Nobre',
+    'Paladino',
+  ],
+  poderes: ['Espada Solar', 'Habitante do Deserto', 'Inimigo de Tenebra'],
+};
+
+export default AZGUER;
