3d84f98127dd61ac245df4c8df95b34fd78866e3
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-07 18:04:18 -0300

    Add Hynnin

---
+++ src/utils/divindades/hyninn.ts
@@ -0,0 +1,10 @@
+import Divindade from './Divindade';
+
+const HYNINN: Divindade = {
+  name: 'Hynnin',
+  racas: ['Hynne', 'Goblin', 'Sílfide'],
+  classes: ['Bardo', 'Bucaneiro', 'Ladino', 'Inventor', 'Nobre'],
+  poderes: ['Farsa do Fingidor', 'Forma de Macaco', 'Golpista Divino'],
+};
+
+export default HYNINN;
