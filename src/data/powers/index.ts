d752d4fe0b78a2a9cdb0f22cc066cd167cd91a81
Isaac Batista <isaacbatst@gmail.com>
2021-03-08 20:57:22 -0300

    fix powers

--- src/data/powers/index.ts
+++
@@ -1,8 +0,0 @@
-import combatPowers from './combatPowers';
-import grantedPowers from './grantedPowers';
-import destinyPowers from './destinyPowers';
-import magicPowers from './spellPowers';
-import tormentaPowers from './tormentPowers';
-import { GeneralPowers } from '../../interfaces/Poderes';
-
-export default generalPowers;
