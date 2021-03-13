b919a65dba7d0f9e011d732ce075cd1b20c77e52
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-13 05:34:50 -0300

    add initial money

--- src/components/Changelog.tsx
+++ src/components/Changelog.tsx
@@ -1,66 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Changelog: React.FC = () => (
-  <div style={{ padding: '0 30px' }}>
-    <h1>Changelog</h1>
-    <p>
-      Segue a lista de mudanças no projeto. Última atualização em 07/03/2021.
-    </p>
-    <h2>Versão 0.2</h2>
-    <ul>
-      <li>
-        Gerando origem aleatória. Os poderes de origem são escolhidos
-        aleatoriamente junto com as perícias, seguindos as regras determinadas
-        pelo sistema.
-      </li>
-      <li>
-        Estamos gerando aleatoriamente se um personagem é devoto ou não. Caso
-        ele seja, é escolhido aleatoriamente o Deus e os poderes concedidos.
-      </li>
-      <li>
-        Filtros de geração permitem que você escolha qual raça/classe deseja
-        gerar (por enquanto ainda só funcionando para nível 1).
-      </li>
-      <li>
-        Os nomes dos personagens são gerados aleatoriamente com base na raça (os
-        nomes usados foram os do escudo do mestre). Nome de Osteon é baseado em
-        uma raça &quot;anterior&quot;, ou seja, a raça do personagem antes dele
-        morrer.
-      </li>
-      <li>
-        Adicionamos um gerador aleatório de itens iniciais com base nas
-        proficiências (mas ainda não estão todos os itens cadastrados).
-      </li>
-      <li>
-        Suporte as seguintes classes: Arcanista, Bardo, Druida e Clérigo.
-        Incluindo geração de magias iniciais.
-      </li>
-    </ul>
-    <h2>Versão 0.1</h2>
-    <ul>
-      <li>
-        Adicionamos a funcionalidade basica de gerar uma ficha de nível 1.
-      </li>
-      <li>
-        Geração aleatório de atributos, considerando os modificadores
-        corretamente.
-      </li>
-      <li>
-        Habilidades de raça ou classe que garantam modificações de atributo e/ou
-        novas perícias já são automaticamente consideradas.
-      </li>
-      <li>Escolha aleatória de perícias (respeitando regras da classe).</li>
-      <li>
-        Suporte as seguintes classes: Bárbaro, Bucaneiro, Caçador, Cavaleiro,
-        Guerreiro, Inventor, Ladino, Lutador, Nobre e Paladino.
-      </li>
-      <li>
-        Suporte as seguintes raças: Anão, Dahllan, Elfo, Goblin, Golem, Humano,
-        Hynne, Kliren, Lefeu, Medusa, Minotauro, Osteon, Qareen, Sereia,
-        Sílfide, Suraggel e Trog.
-      </li>
-    </ul>
-  </div>
-);
+const Changelog: React.FC = () => {
+  useEffect(() => {
+    const footer = document.getElementById('bottom');
+    if (footer) {
+      footer.style.position = 'absolute';
+    }
+  });
+  return (
+    <div style={{ padding: '0 30px' }}>
+      <h1>Changelog</h1>
+      <p>
+        Segue a lista de mudanças no projeto. Última atualização em 07/03/2021.
+      </p>
+      <h2>Versão 0.2</h2>
+      <ul>
+        <li>
+          Gerando origem aleatória. Os poderes de origem são escolhidos
+          aleatoriamente junto com as perícias, seguindos as regras determinadas
+          pelo sistema.
+        </li>
+        <li>
+          Estamos gerando aleatoriamente se um personagem é devoto ou não. Caso
+          ele seja, é escolhido aleatoriamente o Deus e os poderes concedidos.
+        </li>
+        <li>
+          Filtros de geração permitem que você escolha qual raça/classe deseja
+          gerar (por enquanto ainda só funcionando para nível 1).
+        </li>
+        <li>
+          Os nomes dos personagens são gerados aleatoriamente com base na raça
+          (os nomes usados foram os do escudo do mestre). Nome de Osteon é
+          baseado em uma raça &quot;anterior&quot;, ou seja, a raça do
+          personagem antes dele morrer.
+        </li>
+        <li>
+          Adicionamos um gerador aleatório de itens iniciais com base nas
+          proficiências (mas ainda não estão todos os itens cadastrados).
+        </li>
+        <li>
+          Suporte as seguintes classes: Arcanista, Bardo, Druida e Clérigo.
+          Incluindo geração de magias iniciais.
+        </li>
+      </ul>
+      <h2>Versão 0.1</h2>
+      <ul>
+        <li>
+          Adicionamos a funcionalidade basica de gerar uma ficha de nível 1.
+        </li>
+        <li>
+          Geração aleatório de atributos, considerando os modificadores
+          corretamente.
+        </li>
+        <li>
+          Habilidades de raça ou classe que garantam modificações de atributo
+          e/ou novas perícias já são automaticamente consideradas.
+        </li>
+        <li>Escolha aleatória de perícias (respeitando regras da classe).</li>
+        <li>
+          Suporte as seguintes classes: Bárbaro, Bucaneiro, Caçador, Cavaleiro,
+          Guerreiro, Inventor, Ladino, Lutador, Nobre e Paladino.
+        </li>
+        <li>
+          Suporte as seguintes raças: Anão, Dahllan, Elfo, Goblin, Golem,
+          Humano, Hynne, Kliren, Lefeu, Medusa, Minotauro, Osteon, Qareen,
+          Sereia, Sílfide, Suraggel e Trog.
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Changelog;
