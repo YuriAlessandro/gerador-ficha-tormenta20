c7bbe51ff842f2a0afa227709d249827d1e4bd25
Yuri Martins <yuri.alessandro@hotmail.com>
2021-03-14 07:08:23 -0300

    Presentation text

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Select from 'react-select';
+import Fade from '@material-ui/core/Fade';
 import RACAS from '../data/racas';
 import CLASSES from '../data/classes';
 import SelectOptions from '../interfaces/SelectedOptions';
@@ -33,12 +34,21 @@ const MainScreen: React.FC = () => {
   });
 
   const [randomSheet, setRandomSheet] = React.useState<CharacterSheet>();
+  const [showPresentation, setShowPresentation] = React.useState(true);
 
   const onClickGenerate = () => {
     const footer = document.getElementById('bottom');
+    const presentation = document.getElementById('presentation');
     if (footer) {
       footer.style.position = 'relative';
     }
+    if (presentation) {
+      setInterval(() => {
+        presentation.style.opacity = '0';
+        presentation.style.display = 'none';
+      }, 200);
+    }
+    setShowPresentation(false);
     const anotherRandomSheet = generateRandomSheet(selectedOptions);
     setRandomSheet(anotherRandomSheet);
   };
@@ -60,6 +70,34 @@ const MainScreen: React.FC = () => {
 
   return (
     <div id='main-screen'>
+      <Fade in={showPresentation}>
+        <div id='presentation'>
+          <p>
+            “Khalmyr tem o tabuleiro, mas quem move as peças é Nimb”. Deixe que
+            Nimb toque o seu destino de jogo com o Fichas de Nimb.
+          </p>
+          <p>
+            <strong>Fichas de Nimb</strong> é um gerador de fichas aleátorios
+            para o sistema Tormenta 20. Com ele, é possível gerar fichas de
+            raças e classes aleatórias, e ter sua ficha montada
+            instanteneamente. Todos as características de uma ficha de Tormenta
+            20 serão gerados aleatoriamente: atributos, perícias, origem,
+            divindades, magias, etc. Tudo respeitando as regras oficiais do
+            jogo.
+          </p>
+          <p>
+            Fichas de Nimb é uma ótima pedida para jogadores que queiram
+            experimentar um pouco de aleatoriedade ou não gastar muito tempo
+            criando a ficha, e principalmente para mestres que queriam gerar
+            seus NPCs de forma rápida e indolor.
+          </p>
+          <p>
+            Para isso, adicionamos também a opção de filtro de raça, classe e
+            nivel, para que o mestre possa gerar NPCs conforme a narrativa
+            requisite.
+          </p>
+        </div>
+      </Fade>
       <div className='filterArea'>
         <Select
           className='filterSelect'
