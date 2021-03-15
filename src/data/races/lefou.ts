c22475f93b400b0e98768b355752a06a354b69db
Isaac Batista <isaac.batst@gmail.com>
2021-03-14 19:07:21 -0600

    add lefou abilities

--- src/data/races/lefou.ts
+++ src/data/races/lefou.ts
@@ -19,6 +19,18 @@ const LEFOU: Race = {
     AHARADAK: 2,
     THWOR: 1,
   },
+  abilities: [
+    {
+      name: 'Cria da Tormenta',
+      description:
+        'Você é uma criatura do tipo monstro e recebe +5 em testes de resistência contra efeitos causados por lefeu e pela Tormenta.',
+    },
+    {
+      name: 'Deformidade',
+      description:
+        'Todo lefou possui defeitos físicos que, embora desagradáveis, conferem certas vantagens. Você recebe +2 em duas perícias a sua escolha. Cada um desses bônus conta como um poder da Tormenta. Você pode trocar um desses bônus por um poder da Tormenta a sua escolha. Esta habilidade não causa perda de Carisma. (Não inclusos)',
+    },
+  ],
 };
 
 export default LEFOU;
