14d241e83c180c822ef2f4c9fb021b8c238b5984
Isaac Batista <isaac.batst@gmail.com>
2021-03-14 18:17:39 -0600

    add dahllan abilities

--- src/data/races/dahllan.ts
+++ src/data/races/dahllan.ts
@@ -73,6 +73,16 @@ const DAHLLAN: Race = {
         });
       },
     },
+    {
+      name: 'Armadura de Allihanna',
+      description:
+        'Você pode gastar uma ação de movimento e 1 PM para transformar sua pele em casca de árvore, recebendo +2 na Defesa até o fim da cena.',
+    },
+    {
+      name: 'Empatia Selvagem',
+      description:
+        'Você pode se comunicar com animais por meio de linguagem corporal e vocalizações. Você pode usar Adestramento para mudar atitude e pedir favores de animais (veja Diplomacia, na página 117). Caso receba esta habilidade novamente, recebe +2 em Adestramento.',
+    },
   ],
 };
 
