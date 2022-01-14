9d0c6d499f41f8a247ac1b72c447b0df714382b8
Yuri Martins <yuri.alessandro@hotmail.com>
2022-01-14 12:01:16 -0300

    add withRouter to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import { Switch, Route, useHistory, withRouter } from 'react-router-dom';
 import {
   FormControlLabel,
   FormGroup,
@@ -209,4 +209,4 @@ function App(): JSX.Element {
   );
 }
 
-export default App;
+export default withRouter(App);
