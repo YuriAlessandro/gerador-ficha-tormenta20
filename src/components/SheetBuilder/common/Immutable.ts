3f37cd7b9d9b18df8c2cea76960c2f6b102dea04
Isaac Batista <isaacbatst@gmail.com>
2023-06-29 13:32:41 -0300

    refactor immutable func

--- src/components/SheetBuilder/common/Immutable.ts
+++ src/components/SheetBuilder/common/Immutable.ts
@@ -2,12 +2,7 @@ export const updateItemByIndex = <Type>(
   prevArray: Type[],
   updatedItem: Type,
   indexToUpdate: number
-) => {
-  const updatedGroups = prevArray.map((group, index) => {
-    if (index === indexToUpdate) {
-      return updatedItem;
-    }
-    return group;
-  });
-  return updatedGroups;
-};
+) =>
+  prevArray.map((item, index) =>
+    index === indexToUpdate ? updatedItem : item
+  );
