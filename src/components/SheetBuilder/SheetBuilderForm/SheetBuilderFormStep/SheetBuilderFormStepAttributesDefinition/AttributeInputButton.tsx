844d9bcd0e0e3b373a2b82137ed397aa35fc5720
Isaac Batista <isaacbatst@gmail.com>
2023-06-29 13:23:36 -0300

    refactor attribute input classes

--- src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/AttributeInputButton.tsx
+++ src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/AttributeInputButton.tsx
@@ -10,21 +10,16 @@ const AttributeInputButton = ({
   children,
   side,
 }: PropsWithChildren<Props>) => {
-  let className: ComponentProps<'div'>['className'] = '';
-
-  if (side === 'left') {
-    className = 'rounded-tl-2xl rounded-bl-2xl';
-  }
-
-  if (side === 'right') {
-    className = 'rounded-tr-2xl rounded-br-2xl';
-  }
+  const sidesClasses = {
+    right: 'rounded-tl-2xl rounded-bl-2xl',
+    left: 'rounded-tr-2xl rounded-br-2xl',
+  };
 
   return (
     <button
       type='button'
       onClick={onClick}
-      className={`${className} bg-white text-slate-900 px-3 py-2
+      className={`${sidesClasses[side]} bg-white text-slate-900 px-3 py-2
       hover:bg-stone-100 active:opacity-90`}
     >
       {children}
