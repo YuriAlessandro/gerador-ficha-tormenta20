9a27725f60dba7414263273ad27d6ab04b0100a7
Isaac Batista <isaacbatst@gmail.com>
2023-06-29 13:06:30 -0300

    export attributes definition

--- src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/SheetBuilderFormStepAttributesDefinition.tsx
+++ src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/SheetBuilderFormStepAttributesDefinition.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { Attribute } from 't20-sheet-builder';
 import AttributeInput from './AttributeInput';
 import { SheetBuilderFormStepAttributesDefinitionProvider } from './SheetBuilderFormStepAttributesDefinitionContext';
-
-const attributes: Attribute[] = [
-  'strength',
-  'dexterity',
-  'constitution',
-  'intelligence',
-  'wisdom',
-  'charisma',
-];
+import { attributes } from '../../../common/Attributes';
 
 const SheetBuilderFormStepAttributesDefinition = () => (
   <SheetBuilderFormStepAttributesDefinitionProvider>
---
+++ src/components/SheetBuilder/common/Attributes.ts
@@ -0,0 +1,10 @@
+import { Attribute } from 't20-sheet-builder';
+
+export const attributes: Attribute[] = [
+  'strength',
+  'dexterity',
+  'constitution',
+  'intelligence',
+  'wisdom',
+  'charisma',
+];
