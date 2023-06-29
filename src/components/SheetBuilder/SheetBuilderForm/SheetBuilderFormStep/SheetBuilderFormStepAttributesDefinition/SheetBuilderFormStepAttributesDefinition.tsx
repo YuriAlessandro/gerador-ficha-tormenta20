94ba46d08a0e0f18b86bbfd857b9ec6d67e1a163
Isaac Batista <isaacbatst@gmail.com>
2023-06-29 13:05:52 -0300

    add map to print attributes

--- src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/SheetBuilderFormStepAttributesDefinition.tsx
+++ src/components/SheetBuilder/SheetBuilderForm/SheetBuilderFormStep/SheetBuilderFormStepAttributesDefinition/SheetBuilderFormStepAttributesDefinition.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Attribute } from 't20-sheet-builder';
 import AttributeInput from './AttributeInput';
 import { SheetBuilderFormStepAttributesDefinitionProvider } from './SheetBuilderFormStepAttributesDefinitionContext';
+
+const attributes: Attribute[] = [
+  'strength',
+  'dexterity',
+  'constitution',
+  'intelligence',
+  'wisdom',
+  'charisma',
+];
 
 const SheetBuilderFormStepAttributesDefinition = () => (
   <SheetBuilderFormStepAttributesDefinitionProvider>
     <form className='flex flex-row flex-wrap justify-center gap-4'>
-      <AttributeInput attribute='strength' />
-      <AttributeInput attribute='dexterity' />
-      <AttributeInput attribute='constitution' />
-      <AttributeInput attribute='intelligence' />
-      <AttributeInput attribute='wisdom' />
-      <AttributeInput attribute='charisma' />
+      {attributes.map((attribute) => (
+        <AttributeInput key={attribute} attribute={attribute} />
+      ))}
     </form>
   </SheetBuilderFormStepAttributesDefinitionProvider>
 );
