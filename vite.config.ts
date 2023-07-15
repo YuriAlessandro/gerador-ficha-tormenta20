80f7fd762ec24e0df5c6b187c12046836ec05ce7
Yuri Martins <yuri.alessandro@hotmail.com>
2023-07-15 15:06:16 -0300

    fix styling; fix config file

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  base: '/gerador-ficha-tormenta20',
   plugins: [
     react(),
     checker({
