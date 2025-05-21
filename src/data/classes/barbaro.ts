d2bde803d623162de61d460d197bba8d7ae31fa1
Gustavo Carvalho <gustavocarvalho.dev@gmail.com>
2025-05-20 23:13:01 -0300

    Add jda barbaro

--- src/data/classes/barbaro.ts
+++ src/data/classes/barbaro.ts
@@ -42,12 +42,12 @@ const BARBARO: ClassDescription = {
   abilities: [
     {
       name: 'Furia',
-      text: 'Você pode gastar 2 PM para invocar uma fúria selvagem, tornando-se temível em combate. Você recebe +2 em testes de ataque e rolagens de dano corpo a corpo, mas não pode fazer nenhuma ação que exija calma e concentração (como usar a perícia Furtividade ou lançar magias). A cada cinco níveis, pode gastar +2 PM para aumentar os bônus em +1. A Fúria termina se, ao fim da rodada, você não tiver atacado nem sido alvo de um efeito (ataque, habilidade, magia...) hostil.',
+      text: 'Você pode gastar 2 PM para invocar uma fúria selvagem. Você recebe +2 em testes de ataque e rolagens de dano corpo a corpo, mas não pode fazer nenhuma ação que exija calma e concentração (como usar a perícia Furtividade ou lançar magias). A cada cinco níveis, pode gastar +1 PM para aumentar os bônus em +1. A Fúria termina se, ao fim da rodada, você não tiver atacado nem sido alvo de um efeito (ataque, habilidade, magia...) hostil.',
       nivel: 1,
     },
     {
       name: 'Instinto Selvagem',
-      text: 'Você recebe +1 em Percepção e Reflexos (JÁ CONTABILIZADO). A cada seis níveis, esse bônus aumenta em +1.',
+      text: 'Você recebe +1 em rolagens de dano, Percepção e Reflexos. A cada seis níveis, esse bônus aumenta em +1.',
       nivel: 3,
       action: (sheet: CharacterSheet, substeps: SubStep[]): CharacterSheet => {
         const sheetClone = _.cloneDeep(sheet);
@@ -63,8 +63,8 @@ const BARBARO: ClassDescription = {
         });
 
         substeps.push({
-          name: 'Mente Criminosa',
-          value: `Somando modificador de INT em Ladinagem e Furtividade`,
+          name: 'Instinto Selvagem',
+          value: `+1 em Percepção e Reflexos`,
         });
 
         return _.merge<CharacterSheet, Partial<CharacterSheet>>(sheetClone, {
@@ -79,49 +79,49 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Fúria Titânica',
-      text: 'No 20º nível, o bônus que você recebe nos testes de ataque e rolagens de dano quando usa Fúria é dobrado. Por exemplo, se gastar 8 PM, em vez de um bônus de +5, recebe um bônus de +10',
+      text: 'No 20º nível, o bônus que você recebe nos testes de ataque e rolagens de dano quando usa Fúria é dobrado. Por exemplo, se gastar 5 PM, em vez de um bônus de +5, recebe um bônus de +10',
       nivel: 20,
     },
   ],
   powers: [
     {
       name: 'Alma de Bronze',
-      text: 'Quando entra em fúria, você recebe uma quantidade de pontos de vida temporários igual a metade do seu nível + mod. Força.',
+      text: 'Quando entra em fúria, você recebe uma quantidade de pontos de vida temporários igual ao seu nível + sua Força.',
       requirements: [],
     },
     {
       name: 'Aumento de Atributo',
-      text: 'Você recebe +2 em um atributo a sua escolha (NÃO CONTABILIZADO). Você pode escolher este poder várias vezes. A partir da segunda vez que escolhê-lo para o mesmo atributo, o aumento diminui para +1.',
+      text: 'Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.',
       canRepeat: true,
       requirements: [],
     },
     {
       name: 'Brado Assustador',
-      text: 'Você pode gastar uma ação de movimento e 1 PM para soltar um berro feroz. Todos os inimigos em alcance curto devem fazer um teste de Vontade (CD Car). Um inimigo que falhe fica abalado até o fim da cena. Um inimigo que passe se torna imune a esta habilidade até o fim do dia.',
+      text: 'Você pode gastar uma ação de movimento e 1 PM para soltar um berro feroz. Todos os inimigos em alcance curto ficam vulneráveis até o fim da cena.',
       requirements: [
         [{ type: RequirementType.PERICIA, name: Skill.INTIMIDACAO }],
       ],
     },
     {
       name: 'Crítico Brutal',
-      text: 'Seu multiplicador de crítico com ataques corpo a corpo aumenta em +1. Por exemplo, se fizer um crítico com um machado de batalha, seu multiplicador será x4, em vez de x3.',
+      text: 'Seu multiplicador de crítico com ataques corpo a corpo e de arremesso aumenta em +1. Por exemplo, seu multiplicador com um machado de batalha (normalmente x3) será x4',
       requirements: [[{ type: RequirementType.NIVEL, value: 6 }]],
     },
     {
       name: 'Destruidor',
       text: 'Quando causa dano com uma arma corpo a corpo de duas mãos, você pode rolar novamente qualquer resultado 1 ou 2 das rolagens de dano da arma.',
       requirements: [
-        [{ type: RequirementType.ATRIBUTO, name: 'Força', value: 13 }],
+        [{ type: RequirementType.ATRIBUTO, name: 'Força', value: 1 }],
       ],
     },
     {
       name: 'Espírito Inquebrável',
-      text: 'Enquanto está em fúria, você não fica inconsciente por estar com 0 ou menos pontos de vida (você ainda morre se chegar em um valor negativo igual à metade de seus PV máximos).',
+      text: 'Enquanto está em fúria, você não fica inconsciente por estar com 0 PV ou  menos (você ainda morre se chegar em um valor negativo igual à metade de seus pontos de vida máximos).',
       requirements: [[{ type: RequirementType.PODER, name: 'Alma de Bronze' }]],
     },
     {
       name: 'Esquiva Sobrenatural',
-      text: 'Seus instintos ficam tão apurados que você consegue reagir ao perigo antes que seus sentidos percebam. Você nunca fica surpreendido.',
+      text: 'Seus instintos ficam tão apurados que você consegue reagir ao perigo antes que seus sentidos o percebam. Você nunca fica surpreendido.',
       requirements: [],
     },
     {
@@ -131,12 +131,12 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Frenesi',
-      text: 'Se estiver em fúria e usar a ação atacar para fazer um ataque corpo a corpo, você pode gastar 2 PM para fazer um ataque adicional.',
+      text: 'Uma vez por rodada, se estiver em fúria e usar a ação agredir para fazer um ataque corpo a corpo ou com uma arma de arremesso, você pode gastar 2 PM para fazer um ataque adicional.',
       requirements: [],
     },
     {
       name: 'Fúria da Savana',
-      text: 'Seu deslocamento aumenta em +3m (JÁ CONTABILIZADO). Quando usa Fúria, você aplica o bônus em ataque e dano também a armas de arremesso.',
+      text: 'Seu deslocamento aumenta em +3m. Quando usa Fúria, você aplica o bônus em ataque e dano também a armas de arremesso.',
       requirements: [],
       action: (sheet: CharacterSheet, substeps: SubStep[]): CharacterSheet => {
         const sheetClone = _.cloneDeep(sheet);
@@ -158,7 +158,7 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Golpe Poderoso',
-      text: 'Ao acertar um ataque corpo a corpo, você pode gastar 1 PM para causar um dado de dano extra do mesmo tipo (por exemplo, com um montante, causa +1d6, para um dano total de 3d6; com um machado de guerra, causa +1d12).',
+      text: 'Ao acertar um ataque corpo a corpo ou com uma arma de arremesso, você pode gastar 1 PM para causar um dado de dano extra do mesmo tipo (por exemplo, com um montante, causa +1d6, para um dano total de 3d6; com um machado de guerra, causa +1d12).',
       requirements: [],
     },
     {
@@ -168,12 +168,12 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Investida Imprudente',
-      text: 'Quando faz uma investida, você pode aumentar sua penalidade em Defesa pela investida para –5 para receber um bônus de +1d8 na rolagem de dano deste ataque.',
+      text: 'Quando faz uma investida, você pode aumentar sua penalidade em Defesa pela investida para –5 para receber um bônus de +1d12 na rolagem de dano deste ataque.',
       requirements: [],
     },
     {
       name: 'Pele de Aço',
-      text: 'O bônus de Pele de Ferro aumenta para +5.',
+      text: 'O bônus de Pele de Ferro aumenta para +8.',
       requirements: [
         [
           { type: RequirementType.PODER, name: 'Pele de Ferro' },
@@ -183,7 +183,7 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Pele de Ferro',
-      text: 'Você recebe +2 na Defesa, mas apenas se não estiver usando armadura pesada (NÃO CONTABILIZADO).',
+      text: 'Você recebe +4 na Defesa, mas apenas se não estiver usando armadura pesada.',
       requirements: [],
     },
     {
@@ -193,34 +193,35 @@ const BARBARO: ClassDescription = {
     },
     {
       name: 'Superstição',
-      text: 'Você odeia magia, o que faz com que seja mais resistente a ela. Você recebe resistência a magia +2.',
+      text: 'Você odeia magia, o que faz com que seja mais resistente a ela. Você recebe resistência a magia +5.',
       requirements: [],
     },
     {
       name: 'Totem Espiritual',
-      text: 'Você soma seu bônus de Sabedoria no seu total de pontos de mana (JÁ CONTABILIZADO). Escolha um animal totêmico (veja o quadro). Você pode lançar uma magia definida pelo animal escolhido (atributo-chave Sabedoria).',
+      text: 'Você soma seu bônus de Sabedoria no seu total de pontos de mana. Escolha um animal totêmico (veja o quadro). Você aprende e pode lançar uma magia definida pelo animal escolhido (atributo-chave Sabedoria) e pode lançá-la mesmo em fúria..',
       requirements: [
         [
-          { type: RequirementType.ATRIBUTO, name: 'Sabedoria', value: 13 },
+          { type: RequirementType.ATRIBUTO, name: 'Sabedoria', value: 1 },
           { type: RequirementType.NIVEL, value: 4 },
         ],
       ],
-      action: (sheet: CharacterSheet, substeps: SubStep[]): CharacterSheet => {
+      action: (sheet: CharacterSheet): CharacterSheet => {
         const sheetClone = _.cloneDeep(sheet);
-
-        substeps.push({
-          name: 'Totem Espiritual',
-          value: `Somando modificador de SAB no total de mana.`,
+        const pmModifier = sheetClone.pmModifier || [];
+        pmModifier.push({
+          source: 'Totem Espiritual',
+          type: 'Attribute',
+          attribute: Atributo.SABEDORIA,
         });
 
         return _.merge<CharacterSheet, Partial<CharacterSheet>>(sheetClone, {
-          pm: sheet.pm + sheet.atributos.Sabedoria.mod,
+          pmModifier,
         });
       },
     },
     {
       name: 'Vigor Primal',
-      text: 'Você pode gastar uma ação de movimento e 1 PM para recuperar 1d12 pontos de vida. Para cada 2 PM extras que você gastar, cura +1d12 PV (pode gastar 3 PM para recuperar 2d12 PV, 5 PM para recuperar 3d12 PV e assim por diante).',
+      text: 'Você pode gastar uma ação de movimento e uma quantidade de PM limitada por sua Constituição. Para cada PM que gastar, você recupera 1d12 pontos de vida',
       requirements: [],
     },
   ],
